74cb1a0921a26f70b39a459bd87487ca76242eeb
michelelongo97 <m.longo7x@gmail.com>
2025-04-18 16:58:39 +0200

    feat: add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 function App() {
   const [politicans, setPoliticans] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
@@ -10,11 +11,28 @@ function App() {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  const filteredPoliticians = useMemo(() => {
+    return politicans.filter((politician) => {
+      const isInName = politician.name
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      const isInBio = politician.biography
+        .toLowerCase()
+        .includes(search.toLowerCase());
+      return isInName || isInBio;
+    });
+  }, [politicans, search]);
+
   return (
     <div>
       <h1>Lista Politici</h1>
-
-      {politicans.map((politician) => (
+      <input
+        type="text"
+        placeholder="Cerca un politico"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {filteredPoliticians.map((politician) => (
         <div key={politician.id}>
           <h3>{politician.name}</h3>
           <img src={politician.image} alt={politician.name} />
